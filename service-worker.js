662caee9b73cc0c19cf1ef5354cd6348c59f3078
carlinhosdesousaramiresveron-dot <carlinhosdesousaramiresveron@gmail.com>
2025-11-17 14:12:57 -0300

    Update service worker cache and event listeners

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("acdtreino-cache").then((cache) => {
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("muscleup-v1").then(cache => {
       return cache.addAll([
         "index.html",
-        "signup.html",
-        "treino.html",
-        "manifest.json",
-        "icon.png"
+        "style.css",
+        "script.js",
+        "icone-192.png",
+        "icone-512.png",
+        "avatar.png",
+        "treino-braco.png",
+        "treino-peito.png",
+        "treino-perna.png",
+        "treino-costas.png",
+        "treino-abdomen.png",
+        "treino-aquecimento.png"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
     })
   );
 });
